a028c009211bef979a992194d583fdc06994849e
Naveed Aheer <naveed@pittboss.io>
2023-07-04 12:12:30 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import "./App.css";
 
 function App() {
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [bmiValue, setBmiValue] = useState(0);
+  const [errors, setErrors] = useState({});
+
+  const handleCalculate = (e) => {
+    e.preventDefault();
+    setErrors(validate())
+    if (Object.keys(validate()).length === 0)
+      setBmiValue(weight / (height ** 2))
+  }
+
+  const validate = () => {
+    const errors = {}
+
+    if (!height) {
+      errors.heightError = "Please enter height";
+    }
+    if (!weight) {
+      errors.weightError = "Please enter weight";
+    }
+    console.log(errors);
+    return errors;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="main">
+        <div className="app__container">
+          <h1>BMI Calculator</h1>
+          <form className="form__container">
+            <label htmlFor="weight">Weight (KG)</label>
+            <input type="number" name="weight" id="weight" style={errors.weightError && {outline:"1px solid red"}} onChange={(e) => setWeight(e.target.value)} />
+            {errors.weightError && <p style={{ color: "red", marginBottom: "10px" }}>{errors.weightError}</p>}
+            <label htmlFor="height">Height (Meters)</label>
+            <input type="number" name="height" id="height" style={errors.heightError && {outline:"1px solid red"}} onChange={(e) => setHeight(e.target.value)} />
+            {errors.heightError && <p style={{ color: "red", marginBottom: "10px" }}>{errors.heightError}</p>}
+            <button onClick={handleCalculate}>Calculate</button>
+          </form>
+          <div className="result">
+            {
+              bmiValue && <p className="result">BMI VALUE: <b>{bmiValue.toFixed(3)}</b></p>
+            }
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
